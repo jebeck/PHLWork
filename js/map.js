62b4947bdd1bcab066a840fc58c1f5e9d29db19a
Jana E. Beck <jana.eliz.beck@gmail.com>
2013-09-28 18:13:13 -0400

    step 1 of redoing project: in current state will highlight counties and user's home census tract after search

--- js/map.js
+++ js/map.js
@@ -1,110 +1,73 @@
-var feature;
-
 // Latitude first
-var map = L.map('map', {zoomControl: false}).setView([39.95, -75.17], 9);
+var map = L.map('map').setView([39.95, -75.17], 9);
 
 L.tileLayer('http://tile.stamen.com/toner-lite/{z}/{x}/{y}.jpg', {
-	attribution: '<a id="home-link" target="_top" href="../">Map tiles</a> by <a target="_top" href="http://stamen.com">Stamen Design</a>, under <a target="_top" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>',
-	maxZoom: 9,
-	minZoom: 9
+	attribution: '<a id="home-link" target="_top" href="../">Map tiles</a> by <a target="_top" href="http://stamen.com">Stamen Design</a>, under <a target="_top" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>'
 }).addTo(map);
 
 var counties = {
-	"42101": "Philadelphia, PA",
-	"42045": "Delaware, PA",
-	"42091": "Montgomery, PA",
-	"42017": "Bucks, PA",
-	"42029": "Chester, PA",
-	"10003": "New Castle, DE",
-	"34015": "Gloucester, NJ",
-	"34007": "Camden, NJ",
-	"34033": "Salem, NJ",
-	"34005": "Burlington, NJ",
-	"24015": "Cecil, MD"
+	"42101": "Philadelphia County, PA",
+	"42045": "Delaware County, PA",
+	"42091": "Montgomery County, PA",
+	"42017": "Bucks County, PA",
+	"42029": "Chester County, PA",
+	"10003": "New Castle County, DE",
+	"34015": "Gloucester County, NJ",
+	"34007": "Camden County, NJ",
+	"34033": "Salem County, NJ",
+	"34005": "Burlington County, NJ",
+	"24015": "Cecil County, MD"
 };
 
+// add an SVG for D3 to use as a Leaflet overlayPane to display the 11 counties in the Phila. metro statistical area
+var svg = d3.select(map.getPanes().overlayPane).append("svg"), 
+// from Bostock's tutorial (http://bost.ocks.org/mike/leaflet/): "The leaflet-zoom-hide class is needed so that the overlay is hidden during Leaflet’s zoom animation; alternatively, you could disable the animation using the zoomAnimation option when constructing the map."
+	g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+// add an SVG for D3 to use as a Leaflet overlayPane to display the user's home census tract
+var svgHT = d3.select(map.getPanes().overlayPane).append("svg"), 
+	gHT = svgHT.append("g").attr("class", "leaflet-zoom-hide");
+
+var countyBounds;
+
+var countyPaths;
+
+var homeTractBounds;
+
+var homeTractPath;
+
+d3.json('json/counties.json', function(collection) {
+
+	countyBounds = d3.geo.bounds(collection);
+		
+	countyPaths = g.selectAll("path")
+		.data(collection.features)
+		.enter()
+		.append("path");
+
+	countyPaths.append("title")
+		.text(function(d) {
+			var county_id = d.external_id;
+			return counties[county_id];
+		});
+
+	map.on("viewreset", resetCounties);
+	resetCounties();
+});
+
+// function to convert Leaflet's lat and long to D3 pixel coordinates
 var project = function(x) {
 	var point = map.latLngToLayerPoint(new L.LatLng(x[1], x[0]));
 	return [point.x, point.y];
 }
 
+// function to convert GeoJSON to SVG
 var path = d3.geo.path().projection(project);
 
-var svg = d3.select(map.getPanes().overlayPane).append("svg"), 
-	g = svg.append("g").attr("class", "leaflet-zoom-hide");
-
-var bounds;
-
-var D3feature;
-
-var centers = new Object();
-
-var returnCentroid = function(json) {
-	centers[json.external_id] = json.centroid;
-};
-
-var getTractCentroid = function(tract) {
-	var base_url = "http://census.ire.org/geo/1.0/boundary-set/tracts/";
-
-	url = base_url + tract;
-	return $.ajax(url, {
-		dataType: "jsonp",
-		jsonpCallback: 'returnCentroid',
-		async: false
-	});
-};
-
-var loadState = function(state) {
-	var fileExt = '-agged-excluded.json';
-	d3.json(state + fileExt, function(json) {
-		// this will be input from search box
-		var tract = userGeoID;
-
-		g.selectAll("circle")
-			.data(json[tract].workTracts)
-			.enter()
-			.append("circle")
-			.attr({
-				'id': function(d) {
-					return 'w' + d.workTract;
-				},
-				"r": function(d) {
-					var total = 0;
-					if (d.industries['goodsProducing']) {
-						total += d.industries['goodsProducing'];
-					}
-					if (d.industries['transTradeUtil']) {
-						total += d.industries['transTradeUtil'];
-					}
-					if (d.industries['allOther']) {
-						total += d.industries['allOther'];
-					}
-					return 5;
-				},
-				"fill": "blue"
-			});
-
-		var wts = json[tract].workTracts;
-
-		for(i = 0; i < wts.length; i++) {
-			var promise = getTractCentroid(wts[i].workTract);
-			promise.success(function(data) {
-				coords = project(data['centroid']['coordinates']);
-				d3.selectAll("#w" + data['external_id']).attr({
-					"cx": coords[0],
-					"cy": coords[1]
-				});
-			});
-		}
-	});
-};
-
-var states = ['DE', 'MD', 'NJ', 'PA'];
-
-// Reposition the SVG to cover the features.
-var reset = function() {
-	var bottomLeft = project(bounds[0]),
-		topRight = project(bounds[1]);
+// set the dimensions of the SVG to match Leaflet zoom
+var resetCounties = function() {
+	var bottomLeft = project(countyBounds[0]),
+		topRight = project(countyBounds[1]);
 
 	svg.attr("width", topRight[0] - bottomLeft[0])
 		.attr("height", bottomLeft[1] - topRight[1])
@@ -113,94 +76,77 @@ var reset = function() {
 
 	g.attr("transform", "translate(" + -bottomLeft[0] + "," + -topRight[1] + ")");
 
-	D3feature.attr("d", path);
+	countyPaths.attr("d", path);
+};
 
-	D3feature.append("title")
+var displayHomeTract = function() {
+
+	coll = {'type': 'FeatureCollection', 'features': userTractData}
+
+	homeTractBounds = d3.geo.bounds(coll);
+
+	homeTractPath = gHT.selectAll("path")
+		.data(userTractData)
+		.enter()
+		.append("path")
+		.attr("class", "tract");
+
+	homeTractPath.append("title")
 		.text(function(d) {
-			var county_id = d.external_id;
-			return counties[county_id];
+			return "Your home census tract.";
 		});
 
-	// TODO: need to adjust position of dots here, but this doesn't work!
-	// if(wts) {
-	// 	for(i = 0; i < wts.length; i++) {
-	// 		coords = project(centers[wts[i]['workTract']]['coordinates']);
-	// 		d3.select("#w" + wts[i]['workTract']).attr({
-	// 			"cx": coords[0],
-	// 			"cy": coords[1]
-	// 		});
-	// 	}
-	// }
+	map.on("viewreset", resetHomeTract);
+	var lat = userTractData[0].centroid.coordinates[1];
+	var lng = userTractData[0].centroid.coordinates[0];
+	map.setZoomAround([lat, lng], 12);
+	resetHomeTract();
 };
 
-// this function mostly taken from Mike Bostock's http://bost.ocks.org/mike/leaflet/ tutorial
-var overlay = function() {
+var resetHomeTract = function() {
+	var bottomLeft = project(homeTractBounds[0]),
+		topRight = project(homeTractBounds[1]);
 
-	d3.json('counties.json', function(collection) {
+	svgHT.attr("width", topRight[0] - bottomLeft[0])
+		.attr("height", bottomLeft[1] - topRight[1])
+		.style("margin-left", bottomLeft[0] + "px")
+		.style("margin-top", topRight[1] + "px");
 
-		bounds = d3.geo.bounds(collection);
-			
-		D3feature = g.selectAll("path")
-			.data(collection.features)
-				.enter().append("path");
+	gHT.attr("transform", "translate(" + -bottomLeft[0] + "," + -topRight[1] + ")");
 
-		map.on("viewreset", reset);
-		reset();
-	});
+	homeTractPath.attr("d", path);
 };
-
-overlay();
 
 // above is Jana's, below is mine 
 
 var userGeoID;
+
 var userTractData;
+
 var returnGeoID = function(json) {
-	console.log("returnGeoID" + json.objects[0].metadata.GEOID10);
+	// adding and renaming object keys for D3
+	json.objects[0]['type'] = "Feature";
+	json.objects[0]['geometry'] = json.objects[0]['simple_shape'];
+	delete json.objects[0]['simple_shape'];
+
+	console.log(json.objects[0]);
 	userGeoID = json.objects[0].metadata.GEOID10;
-	userTractData = json.objects[0].simple_shape;
-	// this is a bit of a jQuery hack
-	$('circle').remove();
-	for (i = 0; i < states.length; i++) {
-		loadState(states[i]);
-	}
+	userTractData = json.objects;
+	displayHomeTract();
 }
 
 var getCensusTract = function(Lat, Lng) {
-	console.log("hello getCensusTract");
+	// console.log("hello getCensusTract");
 	var base_url = "http://census.ire.org/geo/1.0/boundary/?sets=tracts&contains=";
 	url = base_url + Lat + ", " + Lng;
 	$.ajax(url, {
 		dataType: "jsonp",
-		/*jsonpCallback: 'returnGeoID',*/
 		success: returnGeoID
 	});
-	/*$.getJSON('http://census.ire.org/geo/1.0/boundary/?sets=tracts&format=jsonp&callback=func&contains=' + Lat + "," + Lng, function(data) {
-		console.log("IRE CENSUS " + data.objects[0].external_id)
-	})*/
 }
 
-function chooseAddr(lat1, lng1, lat2, lng2, osm_type, centerLat, centerLng) {
-	var loc1 = new L.LatLng(lat1, lng1);
-	var loc2 = new L.LatLng(lat2, lng2);
-	var bounds = new L.LatLngBounds(loc1, loc2);
-
-	if (feature) {
-		map.removeLayer(feature);
-	}
-	if (osm_type == "node") {
-		feature = L.circle( loc1, 25, {color: 'green', fill: false}).addTo(map);
-		map.fitBounds(bounds);
-		map.setZoom(18);
-	} else {
-		var loc3 = new L.LatLng(lat1, lng2);
-		var loc4 = new L.LatLng(lat2, lng1);
-
-		feature = L.polyline( [loc1, loc4, loc2, loc3, loc1], {color: 'red'}).addTo(map);
-		//map.fitBounds(bounds);
-	}
-
-	console.log("Hello world", centerLat, centerLng);
+function chooseAddr(centerLat, centerLng) {
+	// console.log("Hello world", centerLat, centerLng);
 	return getCensusTract(centerLat, centerLng);
 }
 
@@ -214,21 +160,19 @@ function addr_search() {
             bb = val.boundingbox;
             geocodeLat = val.lat;
             geocodeLng = val.lon;
-            console.log("Location BB:", bb);
+            // console.log("Location BB:", bb);
             items.push("<li><a href='#' onclick='chooseAddr(" + bb[0] + ", " + bb[2] + ", " + bb[1] + ", " + bb[3]  + ", \"" + val.osm_type + "\", " + geocodeLat + ", " + geocodeLng + ");return false;'>" + val.display_name + '</a></li>');
         });
 
 		$('#results').empty();
         if (items.length != 0) {
-            $('<p>', { html: "Search results:" }).appendTo('#results');
+            $('<p>', { html: "Search results:" }).prependTo('#results');
             $('<ul/>', {
                 'class': 'my-new-list',
                 html: items.join('')
             }).appendTo('#results');
         } else {
-            $('<p>', { html: "No results found" }).appendTo('#results');
+            $('<p>', { html: "No results found" }).prependTo('#results');
         }
     });
 }
-
-//window.onload = load_map;
